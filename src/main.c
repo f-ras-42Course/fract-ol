35d3b93d7452d669410f785253461d2cc05a2d35
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2023-07-18 14:40:38 +0200

    drawing crosshair on screen (test)

--- src/main.c
+++ src/main.c
@@ -6,11 +6,13 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/16 19:48:09 by fras          #+#    #+#                 */
-/*   Updated: 2023/07/18 14:20:09 by fras          ########   odam.nl         */
+/*   Updated: 2023/07/18 14:40:13 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
+
+void	test_draw_crosshair(mlx_image_t	*image);
 
 int	main(void)
 {
@@ -25,9 +27,22 @@ int	main(void)
 		return (unexpected_crash(mlx), EXIT_FAILURE);
 	if (mlx_image_to_window(mlx, image, 0, 0) == -1)
 		return(unexpected_crash(mlx), EXIT_FAILURE);
-	mlx_put_pixel(image, 720, 450, 0xffffffff);
+	// mlx_put_pixel(image, 720, 450, 0xffffffff);
+	test_draw_crosshair(image);
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
 	printf("MLX Success - terminated.\n");
 	return (0);
+}
+
+void	test_draw_crosshair(mlx_image_t	*image)
+{
+	int i;
+
+	i = 0;
+	while(i != 1440)
+		mlx_put_pixel(image, i++, 450, 0xffffffff);
+	i = 0;
+	while(i != 900)
+		mlx_put_pixel(image, 720, i++, 0xffffffff);
 }
