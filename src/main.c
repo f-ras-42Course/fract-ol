4a2bb5bcaaa14af46d8c68d37f298e4e795d8cd4
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2023-07-18 14:21:08 +0200

    placed white pixel in center of screen

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/16 19:48:09 by fras          #+#    #+#                 */
-/*   Updated: 2023/07/18 12:43:11 by fras          ########   odam.nl         */
+/*   Updated: 2023/07/18 14:20:09 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,17 @@
 int	main(void)
 {
 	mlx_t		*mlx;
-	mlx_image_t	*background;
+	mlx_image_t	*image;
 
 	mlx = mlx_init(1440, 900, "MLX Screen!", false);
 	if (!mlx)
 		return (unexpected_crash(mlx), EXIT_FAILURE);
-	background = mlx_new_image(mlx, 1440, 900);
-	if (!background)
+	image = mlx_new_image(mlx, 1440, 900);
+	if (!image)
 		return (unexpected_crash(mlx), EXIT_FAILURE);
+	if (mlx_image_to_window(mlx, image, 0, 0) == -1)
+		return(unexpected_crash(mlx), EXIT_FAILURE);
+	mlx_put_pixel(image, 720, 450, 0xffffffff);
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
 	printf("MLX Success - terminated.\n");
