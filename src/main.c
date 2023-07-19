92c7549d2c9138d98c70a3ca4d93c557cdfea400
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2023-07-19 18:22:34 +0200

    WASD + Arrows

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/16 19:48:09 by fras          #+#    #+#                 */
-/*   Updated: 2023/07/19 17:33:13 by fras          ########   odam.nl         */
+/*   Updated: 2023/07/19 17:44:48 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,13 +39,17 @@ void	key_hooks(void *param)
 	if (mlx_is_key_down(window->mlx, MLX_KEY_ESCAPE)\
 		|| mlx_is_key_down(window->mlx, MLX_KEY_Q))
 		mlx_close_window(window->mlx);
-	if (mlx_is_key_down(window->mlx, MLX_KEY_W))
+	if (mlx_is_key_down(window->mlx, MLX_KEY_W)\
+		|| mlx_is_key_down(window->mlx, MLX_KEY_UP))
 		window->image->instances->y -= 5;
-	if (mlx_is_key_down(window->mlx, MLX_KEY_A))
+	if (mlx_is_key_down(window->mlx, MLX_KEY_A)\
+		|| mlx_is_key_down(window->mlx, MLX_KEY_LEFT))
 		window->image->instances->x -= 5;
-	if (mlx_is_key_down(window->mlx, MLX_KEY_S))
+	if (mlx_is_key_down(window->mlx, MLX_KEY_S)\
+		|| mlx_is_key_down(window->mlx, MLX_KEY_DOWN))
 		window->image->instances->y += 5;
-	if (mlx_is_key_down(window->mlx, MLX_KEY_D))
+	if (mlx_is_key_down(window->mlx, MLX_KEY_D)\
+		|| mlx_is_key_down(window->mlx, MLX_KEY_RIGHT))
 		window->image->instances->x += 5;
 }
 
