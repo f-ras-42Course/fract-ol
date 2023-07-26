c4ad430b0aed4a03972e98fc28cb3929fddc2f07
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2023-07-26 20:00:56 +0200

    moved key_hooks function to old_functions to rewrite its purpose

--- src/hooks.c
+++ src/hooks.c
@@ -6,7 +6,7 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/26 17:57:06 by fras          #+#    #+#                 */
-/*   Updated: 2023/07/26 18:03:31 by fras          ########   odam.nl         */
+/*   Updated: 2023/07/26 19:59:56 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 bool	load_hooks(t_all *data)
 {
+	key_hooks(&data->window);
 	mlx_close_hook(data->window.mlx, good_bye_X, NULL);
 	return (mlx_loop_hook(data->window.mlx, loop_hooks, data));
 }
@@ -23,28 +24,13 @@ void	loop_hooks(void *param)
 	t_all		*data;
 	data = param;
 
-	key_hooks(&data->window);
 	image_hooks(&data->window, &data->canvas);
 	show_fps();
 }
 
 void	key_hooks(t_mlx_data *window)
 {
-	if (mlx_is_key_down(window->mlx, MLX_KEY_ESCAPE)\
-		|| mlx_is_key_down(window->mlx, MLX_KEY_Q))
-		mlx_close_window(window->mlx);
-	if (mlx_is_key_down(window->mlx, MLX_KEY_W)\
-		|| mlx_is_key_down(window->mlx, MLX_KEY_UP))
-		window->image->instances->y -= 5;
-	if (mlx_is_key_down(window->mlx, MLX_KEY_A)\
-		|| mlx_is_key_down(window->mlx, MLX_KEY_LEFT))
-		window->image->instances->x -= 5;
-	if (mlx_is_key_down(window->mlx, MLX_KEY_S)\
-		|| mlx_is_key_down(window->mlx, MLX_KEY_DOWN))
-		window->image->instances->y += 5;
-	if (mlx_is_key_down(window->mlx, MLX_KEY_D)\
-		|| mlx_is_key_down(window->mlx, MLX_KEY_RIGHT))
-		window->image->instances->x += 5;
+//new function
 }
 
 void	image_hooks(t_mlx_data *window, t_canvas *canvas)
