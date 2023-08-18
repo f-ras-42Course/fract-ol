1104e9f1fed395be7434248a4631160fb45bc77b
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2023-08-18 16:51:36 +0200

    refator: changed argument name to reduce code-line size

--- src/navigate.c
+++ src/navigate.c
@@ -6,7 +6,7 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/08/18 13:43:51 by fras          #+#    #+#                 */
-/*   Updated: 2023/08/18 16:27:21 by fras          ########   odam.nl         */
+/*   Updated: 2023/08/18 16:44:03 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,32 +26,28 @@ void	image_move_keys(mlx_key_data_t keydata, t_canvas *canvas)
 		canvas->x_coordinate_zero += (WIDTH * 0.125) * canvas->x_increments;
 }
 
-void	image_zoom_keys(mlx_key_data_t keydata, t_canvas *canvas)
+void	image_zoom_keys(mlx_key_data_t keydata, t_canvas *cvs)
 {
-	double	x_midscreen_canvas_value;
-	double	y_midscreen_canvas_value;
-	
-	x_midscreen_canvas_value =\
-		canvas->x_coordinate_zero + ((WIDTH / 2) * canvas->x_increments);
-	y_midscreen_canvas_value =\
-		canvas->y_coordinate_zero - ((HEIGHT / 2) * canvas->y_decrements);
+	double	x_start_size;
+    double	y_start_size;
+
+	x_start_size = (WIDTH * cvs->x_increments);
+	y_start_size = (HEIGHT * cvs->y_decrements);
 	if (keydata.key == MLX_KEY_MINUS)
 	{
-		canvas->x_increments *= canvas->minus_zoom;
-		canvas->y_decrements *= canvas->minus_zoom;
+		cvs->x_increments *= cvs->minus_zoom;
+		cvs->y_decrements *= cvs->minus_zoom;
 	}
 	if (keydata.key == MLX_KEY_EQUAL)
 	{
-		canvas->x_increments *= canvas->plus_zoom;
-		canvas->y_decrements *= canvas->plus_zoom;
+		cvs->x_increments *= cvs->plus_zoom;
+		cvs->y_decrements *= cvs->plus_zoom;
 	}
-	canvas->x_coordinate_zero =\
-		x_midscreen_canvas_value - ((WIDTH / 2) * canvas->x_increments);
-	canvas->y_coordinate_zero =\
-		y_midscreen_canvas_value + ((HEIGHT / 2) * canvas->y_decrements);
+	cvs->x_coordinate_zero += (x_start_size - (WIDTH * cvs->x_increments)) / 2;
+	cvs->y_coordinate_zero -= (y_start_size - (HEIGHT * cvs->y_decrements)) / 2;
 }
 
-void    image_zoom_mouse(double ydelta, mlx_t *mlx, t_canvas *canvas)
+void    image_zoom_mouse(double ydelta, mlx_t *mlx, t_canvas *cvs)
 {
     int32_t	x_mouse_pos;
     int32_t	y_mouse_pos;
@@ -61,21 +57,21 @@ void    image_zoom_mouse(double ydelta, mlx_t *mlx, t_canvas *canvas)
     x_mouse_pos = 0;
     y_mouse_pos = 0;
     mlx_get_mouse_pos(mlx, &x_mouse_pos, &y_mouse_pos);
-	canvas->x_coordinate_zero += (x_mouse_pos - (WIDTH / 2)) * canvas->x_increments;
-	canvas->y_coordinate_zero -= (y_mouse_pos - (HEIGHT / 2)) * canvas->y_decrements;
-	x_start_size = (WIDTH * canvas->x_increments);
-	y_start_size = (HEIGHT * canvas->y_decrements);
+	cvs->x_coordinate_zero += (x_mouse_pos - (WIDTH / 2)) * cvs->x_increments;
+	cvs->y_coordinate_zero -= (y_mouse_pos - (HEIGHT / 2)) * cvs->y_decrements;
+	x_start_size = (WIDTH * cvs->x_increments);
+	y_start_size = (HEIGHT * cvs->y_decrements);
     if (ydelta > 0)
     {
-        canvas->x_increments *= canvas->plus_zoom;
-        canvas->y_decrements *= canvas->plus_zoom;
+        cvs->x_increments *= cvs->plus_zoom;
+        cvs->y_decrements *= cvs->plus_zoom;
     }
     if (ydelta < 0)
     {
-        canvas->x_increments *= canvas->minus_zoom;
-        canvas->y_decrements *= canvas->minus_zoom;
+        cvs->x_increments *= cvs->minus_zoom;
+        cvs->y_decrements *= cvs->minus_zoom;
     }
-	canvas->x_coordinate_zero += (x_start_size - (WIDTH * canvas->x_increments)) / 2;
-	canvas->y_coordinate_zero -= (y_start_size - (HEIGHT * canvas->y_decrements)) / 2;
+	cvs->x_coordinate_zero += (x_start_size - (WIDTH * cvs->x_increments)) / 2;
+	cvs->y_coordinate_zero -= (y_start_size - (HEIGHT * cvs->y_decrements)) / 2;
 	mlx_set_mouse_pos(mlx, WIDTH/2, HEIGHT/2);
 }
