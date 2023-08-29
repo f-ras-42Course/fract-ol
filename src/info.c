3ca04df6fb0373d4eae1a5c67e9f42843b97ce0a
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2023-08-29 22:54:13 +0200

    updates menu

--- src/info.c
+++ src/info.c
@@ -6,7 +6,7 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/26 17:57:54 by fras          #+#    #+#                 */
-/*   Updated: 2023/08/29 19:31:35 by fras          ########   odam.nl         */
+/*   Updated: 2023/08/29 22:39:06 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,59 +52,6 @@ void	show_canvas_data(t_canvas canvas)
 	\n-----------------------------------------\n", \
 	canvas.x_coordinate_zero, canvas.y_coordinate_zero, \
 	canvas.x_increments, canvas.y_decrements);
-}
-
-void	show_menu(mlx_t *mlx, mlx_image_t *menu[])
-{
-	int	x;
-	int	y;
-	int	item;
-
-	x = 20;
-	y = 0;
-	item = 0;
-	menu[item++] = mlx_put_string(mlx, get(wasd_arrow_navigate), x, y += 20);
-	menu[item++] = mlx_put_string(mlx, get(plus_min_zoom), x, y += 20);
-	menu[item++] = mlx_put_string(mlx, get(r_restart), x, y += 20);
-	menu[item++] = mlx_put_string(mlx, get(c_canvas), x, y += 20);
-	menu[item++] = mlx_put_string(mlx, get(f_fps), x, y += 20);
-	menu[item++] = mlx_put_string(mlx, get(m_menu), x, y += 20);
-	menu[item++] = mlx_put_string(mlx, get(o_other_color), x, y += 20);
-	menu[item++] = mlx_put_string(mlx, get(p_psychedelic), x, y += 20);
-	menu[item++] = mlx_put_string(mlx, get(u_ultimate_psychedelic), x, y += 20);
-	menu[item++] = mlx_put_string(mlx, get(julia_coordinates), x, y += 20);
-	menu[item++] = mlx_put_string(mlx, get(julia_move1), x, y += 20);
-	menu[item++] = mlx_put_string(mlx, get(julia_move2), x, y += 20);
-	menu[item++] = mlx_put_string(mlx, "Q or ESC: Exit program", x, y += 20);
-}
-
-char	*get(t_menu_lines instruction)
-{
-	if (instruction == wasd_arrow_navigate)
-		return ("WASD or arrows: Move up/left/down/right");
-	if (instruction == plus_min_zoom)
-		return ("-/+ or scroll-wheel: Zoom in/out");
-	if (instruction == r_restart)
-		return ("R: Reset to start");
-	if (instruction == c_canvas)
-		return ("C: Print canvas values");
-	if (instruction == f_fps)
-		return ("F: Show/hide FPS");
-	if (instruction == m_menu)
-		return ("M: Show/hide menu");
-	if (instruction == o_other_color)
-	 	return ("O: Other color");
-	if (instruction == p_psychedelic)
-		return ("P: Toggle psychedelic mode");
-	if (instruction == u_ultimate_psychedelic)
-		return ("U: Toggle ULTIMATE psychedelic mode");
-	if (instruction == julia_coordinates)
-		return ("Left mouse: Get julia coordinates");
-	if (instruction == julia_move1)
-		return ("CTRL + left mouse: Move to julia-");
-	if (instruction == julia_move2)
-		return ("coordinates or back to mandelbrot");
-	return (NULL);
 }
 
 void	show_julia_coordinates(t_all *data, float julia[])
---
+++ src/menu.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   menu.c                                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: fras <fras@student.codam.nl>                 +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/08/29 22:38:16 by fras          #+#    #+#                 */
+/*   Updated: 2023/08/29 22:51:27 by fras          ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fractol.h"
+
+void	show_menu(mlx_t *mlx, mlx_image_t *menu[])
+{
+	int	x;
+	int	y;
+	int	item;
+
+	x = 20;
+	y = 0;
+	item = 0;
+	menu[item++] = mlx_put_string(mlx, get(wasd_arrow_navigate), x, y += 20);
+	menu[item++] = mlx_put_string(mlx, get(plus_min_zoom), x, y += 20);
+	menu[item++] = mlx_put_string(mlx, get(r_restart), x, y += 20);
+	menu[item++] = mlx_put_string(mlx, get(c_canvas), x, y += 20);
+	menu[item++] = mlx_put_string(mlx, get(f_fps), x, y += 20);
+	menu[item++] = mlx_put_string(mlx, get(m_menu), x, y += 20);
+	menu[item++] = mlx_put_string(mlx, get(o_other_color), x, y += 20);
+	menu[item++] = mlx_put_string(mlx, get(p_psychedelic), x, y += 20);
+	menu[item++] = mlx_put_string(mlx, get(u_ultimate_psychedelic), x, y += 20);
+	menu[item++] = mlx_put_string(mlx, get(julia_coordinates), x, y += 20);
+	menu[item++] = mlx_put_string(mlx, get(julia_mandelbrot_switch), x, y += 20);
+	menu[item++] = mlx_put_string(mlx, get2(julia_pip), x, y += 20);
+	menu[item++] = mlx_put_string(mlx, get2(q_esp_exit), x, y += 20);
+}
+
+char	*get(t_menu_lines instruction)
+{
+	if (instruction == wasd_arrow_navigate)
+		return ("WASD or arrows: Move up/left/down/right");
+	if (instruction == plus_min_zoom)
+		return ("-/+ or scroll-wheel: Zoom in/out");
+	if (instruction == r_restart)
+		return ("R: Reset to start");
+	if (instruction == c_canvas)
+		return ("C: Print canvas values");
+	if (instruction == f_fps)
+		return ("F: Show/hide FPS");
+	if (instruction == m_menu)
+		return ("M: Show/hide menu");
+	if (instruction == o_other_color)
+	 	return ("O: Other color");
+	if (instruction == p_psychedelic)
+		return ("P: Toggle psychedelic mode");
+	if (instruction == u_ultimate_psychedelic)
+		return ("U: Toggle ULTIMATE psychedelic mode");
+	if (instruction == julia_coordinates)
+		return ("Right mouse: Get julia coordinates");
+	if (instruction == julia_mandelbrot_switch)
+		return ("CTRL + right mouse: Switch julia/mandelbrot");
+	return (NULL);
+}
+
+char	*get2(t_menu_lines instruction)
+{
+	if(instruction == julia_pip)
+		return("Left mouse: Display julia PIP in mandelbrot");
+	if (instruction == q_esp_exit)
+		return ("Q or ESC: Exit program");
+	return (NULL);
+}
